d5f010893bfb812e01c062461f81378e1eb00b98
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-06-17 10:49:11 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        {!! QrCode::size(250)
+            ->format('svg')
+            ->merge(\Illuminate\Support\Facades\Storage::get('/images/ltfrb.png'))
+            ->errorCorrection('H')
+            ->generate('nice one'); !!}
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $image = \Illuminate\Support\Facades\Storage::get('/images/ltfrb.png');
+
+    return view('welcome', compact('image'));
+});
